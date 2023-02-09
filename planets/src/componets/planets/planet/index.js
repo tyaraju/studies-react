87d99bbccb87d668db2df8da89cd34babe08a8e6
Tyaraju Terra <tyarajuterra@hotmail.com>
2023-02-09 20:32:16 -0300

    Finalizando lifecycle

--- planets/src/componets/planets/planet/index.js
+++ planets/src/componets/planets/planet/index.js
@@ -2,22 +2,54 @@ import React from 'react';
 import GrayImg from '../../../shared/gray_image';
 import DescritionWithLink from '../../../shared/descrition_with_link';
 
-const Planet = (props) => {
-  let title;
-  if(props.title_with_underline){
-    title = <h4><u>{props.name}</u></h4>
-  } else {
-    title = <h4>{props.name}</h4>
+async function getSatellites(id) {
+  let response = await fetch(`./api/${id}.json`,{
+    headers : { 
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+     }
+  })
+  let data = await response.json()
+  return data;
+}
+class Planet extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      satellites: []
+    }
   }
-  return (
-    <div onClick={() => props.clickOnPlanet(props.name)}>
-      {title}
-      <DescritionWithLink description={props.description} link={props.link} cta={props.cta}/>
-      <GrayImg alt={props.name} img_url={props.img_url} gray={props.gray}/>
+  componentDidMount() {
+    getSatellites(this.props.id).then(data => {
+      this.setState(state => ({
+        satellites: data['satellites']
+      }))
+    })
+  }
+  render() {
+    let title;
+    if(this.props.title_with_underline){
+      title = <h4><u>{this.props.name}</u></h4>
+    } else {
+      title = <h4>{this.props.name}</h4>
+    }
 
-      
-    </div>
-  )
+    return (
+      <div onClick={() => this.props.clickOnPlanet(this.props.name)}>
+        {title}
+        <DescritionWithLink description={this.props.description} link={this.props.link} cta={this.props.cta}/>
+        <GrayImg alt={this.props.name} img_url={this.props.img_url} gray={this. props.gray}/>
+        <h4>Satélites</h4>
+        <ul>
+        {this.state.satellites.map((satellite, index) => 
+        <li key={index}>Satélite {satellite.name}</li>
+        )}
+        </ul>
+        
+      </div>
+    )
+  }
+  
 }
 
 export default Planet;
